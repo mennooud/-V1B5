0d9e3056384c41d3d55ee916d75685373044c07a
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-29 14:37:42 +0200

    Een functie toegevoegd voor het toevoegen van een kolom, ook oude functie aangepast zodat deze normaliseringstabellen weg kan halen

--- GroupProject/DatabaseGenerate/datachange.py
+++ GroupProject/DatabaseGenerate/datachange.py
@@ -1,2 +1,30 @@
 import PGAdmin as P
+import MongoDB as M
+from pymongo import MongoClient
 
+
+def deletecolumn(cursor, table, columnname, normtablename=None):
+    if normtablename:
+        column = normtablename+columnname+'id'
+    else:
+        column = columnname
+    cursor.execute(f'ALTER TABLE {table} DROP COLUMN {column}')
+    cursor.execute(f'DROP TABLE {normtablename}')
+
+
+def addcolumn(cursor, table, columnname, normtablename=None):
+    if normtablename:
+        column = normtablename+columnname+'id'
+    else:
+        column = columnname
+    P.executequery(cursor, f'ALTER TABLE {table} ADD COLUMN {column}')
+
+
+client = MongoClient()
+db = client.huwebshop
+allproducts = M.getitems(db.products)
+
+connection= P.makeconnection('localhost', 'test', 'postgres', 'broodje123')
+cursor = P.makecursor(connection)
+addcolumn(cursor, allproducts, 'properties.doelgroep', None)
+P.closeconnection(connection, cursor)
