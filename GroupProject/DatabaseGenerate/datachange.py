71afc3cb3cf0d0ad17b05daffc5b2ec949ef0720
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-29 14:54:31 +0200

    De functie voor het toevoegen van kolommen en eventueel normaliseringstabellen verbeterd; werkte eerst niet.

--- GroupProject/DatabaseGenerate/datachange.py
+++ GroupProject/DatabaseGenerate/datachange.py
@@ -14,10 +14,13 @@ def deletecolumn(cursor, table, columnname, normtablename=None):
 
 def addcolumn(cursor, table, columnname, normtablename=None):
     if normtablename:
-        column = normtablename+columnname+'id'
+        P.executequery(cursor, f'CREATE TABLE {normtablename} ({columnname+"id"} SERIAL PRIMARY KEY , {columnname} VARCHAR)')
+        P.executequery(cursor, f'ALTER TABLE {table} ADD COLUMN {normtablename+columnname+"id"} INT')
+        P.executequery(cursor, f'ALTER TABLE {table} ADD FOREIGN KEY ({normtablename+columnname+"id"}) REFERENCES '
+                               f'{normtablename}({columnname+"id"})')
     else:
-        column = columnname
-    P.executequery(cursor, f'ALTER TABLE {table} ADD COLUMN {column}')
+        P.executequery(cursor, f'ALTER TABLE {table} ADD COLUMN {columnname}')
+
 
 
 client = MongoClient()
@@ -26,5 +29,6 @@ allproducts = M.getitems(db.products)
 
 connection= P.makeconnection('localhost', 'test', 'postgres', 'broodje123')
 cursor = P.makecursor(connection)
-addcolumn(cursor, allproducts, 'properties.doelgroep', None)
+addcolumn(cursor, 'products', 'doelgroep', 'doelgroepen')
+connection.commit()
 P.closeconnection(connection, cursor)
